24bed764d20b0496d41449f22cf0be1a61ac2525
IgorALopes <igor9487@gmail.com>
2022-05-02 14:13:47 -0300

    Arredondamentos e atualização no texto

--- Calculadora combustível F1 - Invillia/script.js
+++ Calculadora combustível F1 - Invillia/script.js
@@ -1,3 +1,17 @@
+const round = (num, places) => {
+	if (!("" + num).includes("e")) {
+		return +(Math.round(num + "e+" + places)  + "e-" + places);
+	} else {
+		let arr = ("" + num).split("e");
+		let sig = ""
+		if (+arr[1] + places > 0) {
+			sig = "+";
+		}
+
+		return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + places)) + "e-" + places);
+	}
+}
+
 function reabastecer() {
     let kml = 1.3
     let voltas = document.getElementById('numvoltas')
@@ -13,9 +27,9 @@ function reabastecer() {
     litros = (voltasLitros * kmC) / 1000
     abastecimento = tV / v
 
-        if (litros > 82) {
-            window.alert(`A capacidade de armazenamento do veículo de 82 litros foi excedido (${litros}). Para a realização das voltas pretendidas, favor fornecer uma quantidade menor.`)
-        }
-
-    res.innerHTML = `Serão necessários, no mínimo, ${litros} litros para realizar as ${v} voltas desejadas antes do próximo reabastecimento. Serão necessários ${Math.round(abastecimento)} reabastecimentos para completar a corrida.`
+    if (litros > 82) {
+        window.alert(`A capacidade de armazenamento do veículo de 82 litros foi excedido em ${round(litros - 82, 3).toLocaleString('pt-BR')} litros. Favor estabelecer uma quantidade menor de voltas.`)
+    } else {
+        res.innerHTML = `Serão necessários, no mínimo, ${round(litros, 3).toLocaleString('pt-BR')} litros para realizar as ${v} voltas desejadas antes do próximo reabastecimento. Serão necessários ${Math.round(abastecimento)} reabastecimentos para completar a corrida.`
+    }
 }
